e2f36a3ace56c9568bd971dc3efdcb322c8b7d3a
shakil <36266736+sXakil@users.noreply.github.com>
2019-04-22 16:20:23 +0600

    Updates

---
+++ precache-manifest.23de842a69fb25654ec47bf5fbbf59fc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9328361c0c185102608",
+    "url": "/portfolio/static/css/main.2e31e209.chunk.css"
+  },
+  {
+    "revision": "b9328361c0c185102608",
+    "url": "/portfolio/static/js/main.dedef453.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "639043cdaf47ec8f3d07",
+    "url": "/portfolio/static/js/2.79145ec7.chunk.js"
+  },
+  {
+    "revision": "4305b311c2304c723fb7e297e382ee0e",
+    "url": "/portfolio/static/media/github.4305b311.svg"
+  },
+  {
+    "revision": "6b6ff86734295d5a7016b202f620a123",
+    "url": "/portfolio/static/media/facebook.6b6ff867.svg"
+  },
+  {
+    "revision": "51eef8023872698099537598fbd40e63",
+    "url": "/portfolio/static/media/at.51eef802.svg"
+  },
+  {
+    "revision": "1f55925e52b9e71fd57108874184060d",
+    "url": "/portfolio/static/media/reg.1f55925e.otf"
+  },
+  {
+    "revision": "665da3f4ac02b6fb8b1d1d50c3f78b67",
+    "url": "/portfolio/index.html"
+  }
+];

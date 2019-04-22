32c867732d4d935215c3b51a4d4828505a15bcd9
shakil <36266736+sXakil@users.noreply.github.com>
2019-04-22 16:25:55 +0600

    Updates

---
+++ precache-manifest.623268ad37412da9c83efd4002c60833.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8993ad43fd04e63e685",
+    "url": "/static/css/main.9d93f964.chunk.css"
+  },
+  {
+    "revision": "b8993ad43fd04e63e685",
+    "url": "/static/js/main.d10e262a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "639043cdaf47ec8f3d07",
+    "url": "/static/js/2.79145ec7.chunk.js"
+  },
+  {
+    "revision": "4305b311c2304c723fb7e297e382ee0e",
+    "url": "/static/media/github.4305b311.svg"
+  },
+  {
+    "revision": "6b6ff86734295d5a7016b202f620a123",
+    "url": "/static/media/facebook.6b6ff867.svg"
+  },
+  {
+    "revision": "51eef8023872698099537598fbd40e63",
+    "url": "/static/media/at.51eef802.svg"
+  },
+  {
+    "revision": "1f55925e52b9e71fd57108874184060d",
+    "url": "/static/media/reg.1f55925e.otf"
+  },
+  {
+    "revision": "2d8644adefc79baa824752c25b8fefbb",
+    "url": "/index.html"
+  }
+];
